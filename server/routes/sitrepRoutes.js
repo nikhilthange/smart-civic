aebfb1b796fd2d8defb4d2a09ca738da907eff24
Nikhil Thange <nikhilthange075@gmail.com>
2026-08-27 12:37:20 +0530

    whatsapp cloud error

--- server/routes/sitrepRoutes.js
+++ server/routes/sitrepRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { cacheMiddleware } = require("../middlewares/cacheMiddleware");
+const { getDailySitrep } = require("../controllers/sitrepController");
 
 // GET /api/sitrep/daily (20-second TTL cache)
 router.get("/daily", cacheMiddleware(20), getDailySitrep);
